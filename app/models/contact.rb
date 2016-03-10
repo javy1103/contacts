da4905aae900c0d9831c520d47499b184fbd0bd2
Javier <javy1103@gmail.com>
2016-03-10 14:54:57 -0600

    make it better

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
   has_one :phone, dependent: :destroy
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :phone
